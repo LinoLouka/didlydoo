b2fd2aac8c402db0a40a1120a55f7d2360b9ad5f
LeShib <dibernardonikko@gmail.com>
2023-05-30 10:37:49 +0200

    update an event: in progresse

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,3 @@
-// /* Variables */
-// const allEvents = document.getElementById("events");
-
-// /* Fonctions */
-// // Récupération des informations depuis l'API
-// function fetchData() {
-//   fetch("http://localhost:3000/api/events/")
-//     .then((response) => response.json())
-//     .then((json) => {
-//       console.log(json);
-//       displayEvents(json);
-//     });
-// }
-
-// // Création de l'html et affichage des des tableaux contenant les évènements
-// function displayEvents(data) {
-//   data.forEach((event) => {
-//     const eventDiv = document.createElement("div");
-
-//     eventDiv.innerHTML = `<h2 class="events__title">${event.name}</h2><p class="events__description">${event.description}</p><p class="events__author">${event.author}</p><button class="delete-button" data-id="${event.id}">Delete Event</button>`;
-//     const table = document.createElement("table");
-//     const thead = document.createElement("thead");
-//     const tbody = document.createElement("tbody");
-//     const tr = document.createElement("tr");
-//     const th = document.createElement("th");
-//     th.textContent = "Attendees\\Dates";
-//     tr.appendChild(th);
-//     event.dates.forEach((date) => {
-//         const th = document.createElement("th");
-//         th.textContent = date.date;
-//         tr.appendChild(th);
-//     });
-//     thead.appendChild(tr);
-//     table.appendChild(thead);
-//     event.dates[0].attendees.forEach((attendee) => {
-//         const tr = document.createElement("tr");
-//         const td = document.createElement("td");
-//         td.textContent = attendee.name;
-//         tr.appendChild(td);
-//         event.dates.forEach((date) => {
-//             const td = document.createElement("td");
-//             td.textContent = date.attendees.find(
-//                 (a) => a.name === attendee.name
-//             ).available;
-//             tr.appendChild(td);
-//         });
-//         tbody.appendChild(tr);
-//     });
-//     table.appendChild(tbody);
-//     eventDiv.appendChild(table);
-//     allEvents.appendChild(eventDiv);
-
-//     const deleteButton = eventDiv.querySelector(".delete-button");
-//     deleteButton.addEventListener("click", () => {
-//         const eventId = deleteButton.getAttribute("data-id");
-//         deleteEvent(eventId, eventDiv);
-//     });
-//   });
-// }
-
-// // Fonction de suppression
-// function deleteEvent(eventId, eventDiv) {
-//     fetch(`http://localhost:3000/api/events/${eventId}`, { method: "DELETE" })
-//     .then((response) => {
-//         if (response.ok) {
-//             eventDiv.remove();
-//         } else {
-//             console.error("Erreur lors de la suppression de l'événement");
-//         }
-//     })
-//     .catch((error) => {
-//         console.error("Erreur lors de la suppression de l'événement :", error);
-//     });
-// }
-
-// /* Appels de fonctions */
-// fetchData();
-
-
-/* Evenements */
-
-
 /* Variables */
 const allEvents = document.getElementById("events");
 
@@ -194,14 +112,15 @@ function updateParticipantAvailability(eventId, eventName, participantName, upDa
                     apiDate.attendees.forEach(attend => {
                         if(attend.name === participantName){
                             console.log(eventId, eventName, participantName, upDate, availability);
-                            fetch(`http://localhost:3000/api/events/${eventId}`, {
+                            fetch(`http://localhost:3000/api/events/${eventId}/attend`, {
                                 method: "PATCH",
                                 headers: {
                                     "Content-Type": "application/json"
                                 },
                                 body: JSON.stringify({
-                                    name: eventName, 
-                                    // attendees: [ dates: [ { date: upDate, available: availability } ] ]
+                                    // name: eventName, 
+                                    // attendees: [ { dates: [ { date: upDate, available: availability } ] } ]
+                                    name: participantName,
                                     dates: [ { date: upDate, available: availability } ]
                                 })
                             })
