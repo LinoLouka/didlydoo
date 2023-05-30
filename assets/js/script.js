3551b5a25077feeb08d5ce7f516e19d7936513ae
LeShib <dibernardonikko@gmail.com>
2023-05-30 20:38:52 +0200

    update event: in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+// {
+//     "name": "Michou",
+//     "dates": [
+//       {
+//         "date": "2022-03-17",
+//         "available": true
+//       },
+//       {
+//         "date": "2022-03-18",
+//         "available": false
+//       },
+//       {
+//         "date": "2022-03-21",
+//         "available": true
+//       }
+//     ]
+//   },
+
+
 /* Variables */
 const allEvents = document.getElementById("events");
 
@@ -13,10 +32,85 @@ function fetchData() {
 }
 
 // Création de l'html et affichage des tableaux contenant les événements
+// function displayEvents(data) {
+//     allEvents.innerHTML = "";
+//     data.forEach((event) => {
+//         const eventDiv = document.createElement("div");
+
+//         eventDiv.innerHTML = `<h2 class="events__title">${event.name}</h2><p class="events__description">${event.description}</p><p class="events__author">${event.author}</p><button class="delete-button" data-id="${event.id}">Delete Event</button>`;
+//         const table = document.createElement("table");
+//         const thead = document.createElement("thead");
+//         const tbody = document.createElement("tbody");
+//         const tr = document.createElement("tr");
+//         const th = document.createElement("th");
+//         th.textContent = "Attendees\\Dates";
+//         tr.appendChild(th);
+//         event.dates.forEach((date) => {
+//             const th = document.createElement("th");
+//             th.textContent = date.date;
+//             tr.appendChild(th);
+//         });
+//         thead.appendChild(tr);
+//         table.appendChild(thead);
+//         event.dates[0].attendees.forEach((attendee) => {
+//             const tr = document.createElement("tr");
+//             const td = document.createElement("td");
+//             td.textContent = attendee.name;
+//             tr.appendChild(td);
+//             event.dates.forEach((date) => {
+//                 const td = document.createElement("td");
+//                 td.textContent = date.attendees.find((a) => a.name === attendee.name).available;
+//                 tr.appendChild(td);
+//             });
+//             tbody.appendChild(tr);
+//         });
+//         table.appendChild(tbody);
+//         eventDiv.appendChild(table);
+//         allEvents.appendChild(eventDiv);
+
+//         const deleteButton = eventDiv.querySelector(".delete-button");
+//         deleteButton.addEventListener("click", () => {
+//             const eventId = deleteButton.getAttribute("data-id");
+//             deleteEvent(eventId, eventDiv);
+//         });
+
+//         const form = document.createElement("form");
+//         const nameInput = document.createElement("input");
+//         nameInput.type = "text";
+//         nameInput.placeholder = "Nom du participant";
+//         form.appendChild(nameInput);
+
+//         const buttonDiv = document.createElement("div");
+//         event.dates.forEach((date) => {
+//             const yesButton = document.createElement("button");
+//             yesButton.textContent = "V";
+//             yesButton.addEventListener("click", (e) => {
+//                 e.preventDefault();
+//                 const participantName = nameInput.value;
+//                 updateParticipantAvailability(event.id, event.name, participantName, date.date, true);
+//             });
+//             buttonDiv.appendChild(yesButton);
+
+//             const noButton = document.createElement("button");
+//             noButton.textContent = "X";
+//             noButton.addEventListener("click", (e) => {
+//                 e.preventDefault();
+//                 const participantName = nameInput.value;
+//                 updateParticipantAvailability(event.id, event.name, participantName, date.date, false);
+//             });
+//             buttonDiv.appendChild(noButton);
+//         });
+
+//         form.appendChild(buttonDiv);
+//         eventDiv.appendChild(form);
+//     });
+// }
+
 function displayEvents(data) {
+    allEvents.innerHTML = "";
+    let id = 0;
     data.forEach((event) => {
         const eventDiv = document.createElement("div");
-
         eventDiv.innerHTML = `<h2 class="events__title">${event.name}</h2><p class="events__description">${event.description}</p><p class="events__author">${event.author}</p><button class="delete-button" data-id="${event.id}">Delete Event</button>`;
         const table = document.createElement("table");
         const thead = document.createElement("thead");
@@ -60,30 +154,35 @@ function displayEvents(data) {
         nameInput.placeholder = "Nom du participant";
         form.appendChild(nameInput);
 
-        const buttonDiv = document.createElement("div");
-        event.dates.forEach((date) => {
-            const yesButton = document.createElement("button");
-            yesButton.textContent = "V";
-            yesButton.addEventListener("click", (e) => {
-                e.preventDefault();
-                const participantName = nameInput.value;
-                updateParticipantAvailability(event.id, event.name, participantName, date.date, true);
-            });
-            buttonDiv.appendChild(yesButton);
-
-            const noButton = document.createElement("button");
-            noButton.textContent = "X";
-            noButton.addEventListener("click", (e) => {
-                e.preventDefault();
-                const participantName = nameInput.value;
-                updateParticipantAvailability(event.id, event.name, participantName, date.date, false);
-            });
-            buttonDiv.appendChild(noButton);
+        const checkboxDiv = document.createElement("div");
+        event.dates.forEach((apiDate) => {
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.setAttribute("id", `checkbox ${id}`);
+            id++;
+            checkbox.name = `${apiDate.date}`;
+            // checkbox.addEventListener("change", () => {
+            //     const participantName = nameInput.value;
+            //     const isAvailable = checkbox.checked;
+            //     updateParticipantAvailability(event.id, participantName, apiDate, isAvailable);
+            // });
+            checkboxDiv.appendChild(checkbox);
         });
 
-        form.appendChild(buttonDiv);
+        form.appendChild(checkboxDiv);
+        const submitButton = document.createElement("button");
+        submitButton.textContent = "Submit";
+        submitButton.addEventListener("click", (e) => {
+            e.preventDefault();
+            const participantName = nameInput.value;
+            updateParticipantAvailability(event.id, participantName);
+        });
+        form.appendChild(submitButton);
         eventDiv.appendChild(form);
     });
+    let cb1 = document.getElementById("checkbox 1");
+    console.log(cb1.checked);
+    console.log(cb1.name);
 }
 
 // Fonction de suppression
@@ -102,26 +201,30 @@ function deleteEvent(eventId, eventDiv) {
 }
 
 // Mise à jour de la disponibilité du participant
-function updateParticipantAvailability(eventId, eventName, participantName, upDate, availability) {
+function updateParticipantAvailability(eventId, participantName) {
+    let id = 0;
     fetch("http://localhost:3000/api/events/")
     .then((response) => response.json())
     .then((json) => {
         json.forEach(obj => {
             if(obj.id === eventId){
                 obj.dates.forEach(apiDate => {
+                    // console.log(apiDate);
                     apiDate.attendees.forEach(attend => {
+                        console.log(attend);
+                        let cb = document.getElementById(`checkbox ${id}`);
+                        console.log(cb.name, cb.checked);
+                        id++;
                         if(attend.name === participantName){
-                            console.log(eventId, eventName, participantName, upDate, availability);
+                            console.log(eventId, participantName);
                             fetch(`http://localhost:3000/api/events/${eventId}/attend`, {
                                 method: "PATCH",
                                 headers: {
                                     "Content-Type": "application/json"
                                 },
                                 body: JSON.stringify({
-                                    // name: eventName, 
-                                    // attendees: [ { dates: [ { date: upDate, available: availability } ] } ]
                                     name: participantName,
-                                    dates: [ { date: upDate, available: availability } ]
+                                    dates: [ { date: cb.name, available: cb.checked } ]
                                 })
                             })
                             .then((response) => response.json())
@@ -161,6 +264,7 @@ function updateParticipantAvailability(eventId, eventName, participantName, upDa
                 console.log("ko");
             }
         });
+        displayEvents(json);
     });
 }
 
